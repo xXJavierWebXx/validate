d59d5c141adcf69789b8dbb70fd1adf6764778ab
Javier Peréz <42780297+xXJavierWebXx@users.noreply.github.com>
2018-09-12 11:46:48 -0500

    Create code.php

---
+++ code.php
@@ -0,0 +1,1 @@
+Sub Mensaje() - Msgbox ""HOLA MUNDOS!"",vbInformation,""ESTO ES UN MENSAJE DE EJEMPLO"" - End Sub - Sub MensajeS() - Msgbox ""HOLA MUNDOS!"",vbInformation,""ESTO ES UN MENSAJE DE EJEMPLO"" - End Sub
