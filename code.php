6a368752621155e264297f090378dd5730c1eb22
Javier Peréz <42780297+xXJavierWebXx@users.noreply.github.com>
2018-09-19 11:05:21 -0500

    Update code.php

--- code.php
+++ code.php
@@ -1,1 +1,1 @@
-mod_update *@* Sub loadmod() *@* frm_calcular.op_colaborador.Enabled = true *@* End Sub *@* *@* Function LoadVer() As String *@* LoadVer = "1.5" *@* End Function
+mod_update  *@*  Sub loadmod()  *@*  frm_calcular.op_colaborador.Enabled = true  *@*  End Sub  *@*  Function LoadVer() As String  *@*  LoadVer = "1.5"  *@*  End Function
